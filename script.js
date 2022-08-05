62cfa9e14de4e488c4b9c24f2a9df9038aecf0fc
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-04 23:45:11 -0300

    Resolução do quarto requisito

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let guessColor = document.querySelector('#rgb-color');
+let options = document.querySelectorAll('.ball');
+
+function generateOptionsColor () {
+  for (let index = 0; index < options.length; index += 1) {
+    options[index].style.backgroundColor = `rgb(${Math.random()
+      * 255},${Math.random() * 255},${Math.random() * 255})`;
+    
+  }
+}
 
 window,onload = function initial() {
   guessColor.innerHTML = `(${parseInt(Math.random()
     * 255)}, ${parseInt(Math.random() * 255)}, ${parseInt(Math.random() * 255)})`;
+    generateOptionsColor();
 }
